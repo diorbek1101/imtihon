a5fa8c88fd07b3fdec85df6e06cf001b074ecbe0
diorbek1101 <solijonovdiyorbek@gmail.com>
2023-09-27 12:58:28 +0500

    exam

---
+++ index.js
@@ -0,0 +1,34 @@
+let boxes = document.querySelectorAll(".box");
+let right = document.getElementById("right");
+let left = document.getElementById("left");
+
+for (box of boxes) {
+	box.addEventListener("dragstart", function (e) {
+		let selected = e.target;
+
+		right.addEventListener("dragover", function (e) {
+			e.preventDefault();
+		});
+		right.addEventListener("drop", function (e) {
+			right.appendChild(selected);
+			selected = null;
+		});
+
+		medium.addEventListener("dragover", function (e) {
+			e.preventDefault();
+		});
+		medium.addEventListener("drop", function (e) {
+			medium.appendChild(selected);
+			selected = null;
+		});
+
+		left.addEventListener("dragover", function (e) {
+			e.preventDefault();
+		});
+		left.addEventListener("drop", function (e) {
+			left.appendChild(selected);
+			selected = null;
+		});
+	});
+
+}
